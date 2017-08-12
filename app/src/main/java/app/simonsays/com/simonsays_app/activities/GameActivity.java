5ed341f8803896bdb884f16cb9a0373e0d507e81
Dariel Cruz Hernandez <darielnaturalist@gmail.com>
2017-08-12 10:39:47 -0400

    Finishing the app

--- app/src/main/java/app/simonsays/com/simonsays_app/activities/GameActivity.java
+++ app/src/main/java/app/simonsays/com/simonsays_app/activities/GameActivity.java
@@ -4,7 +4,6 @@ import android.media.MediaPlayer;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.NumberPicker;
